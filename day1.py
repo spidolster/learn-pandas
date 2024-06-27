5ce6018fd48df6255d5702875481aeff8ec137f2
spidolster <spidolster@gmail.com>
2024-06-27 04:40:29 +0000

    Add 4 line of data

--- day1.py
+++ day1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.DataFrame({'Yes':[1],
-                   'No':[2]}, 
-                   index=['Poll'])
+df = pd.DataFrame({'Yes':[1,2,3,4],
+                   'No':[2,4,6,8]}, 
+                   index=['A','B','C','D'])
 print(df)
